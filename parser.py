2aa8c2703e6baddd5a77dae8447ea2863859e491
jiwen xin <kevinxin@scripps.edu>
2019-08-29 10:59:30 -0700

    update parser to include CC, BP, MF

--- parser.py
+++ parser.py
@@ -1,16 +1,17 @@
 import requests
 
 
-def load_data(data_folder):
+def load_data(data_folder=None):
+    # load pathways
     url = 'http://mygene.info/v3/query?q=_exists_:pathway&fields=pathway&fetch_all=TRUE'
     cnt = 0
     total = 1
     pathway_ids = set()
     while cnt < total:
         doc = requests.get(url).json()
-        total = doc['total']
+        if total == 1:
+            total = doc['total']
         cnt += len(doc['hits'])
-        print('current cnt {}'.format(cnt))
         url = 'http://mygene.info/v3/query?scroll_id=' + doc['_scroll_id']
         for _doc in doc['hits']:
             for db, info in _doc['pathway'].items():
@@ -22,4 +23,68 @@ def load_data(data_folder):
                         pathway_ids.add(_id)
                         yield {'_id': _id,
                                db: record['id'],
-                               'name': record['name']}
+                               'name': record['name'],
+                               'type': 'pathway'}
+    # load biological process
+    url = 'http://mygene.info/v3/query?q=_exists_:go.BP&fields=go.BP&fetch_all=TRUE'
+    cnt = 0
+    total = 1
+    pathway_ids = set()
+    while cnt < total:
+        doc = requests.get(url).json()
+        total = doc['total']
+        cnt += len(doc['hits'])
+        url = 'http://mygene.info/v3/query?scroll_id=' + doc['_scroll_id']
+        for _doc in doc['hits']:
+            info = _doc['go']['BP']
+            if isinstance(info, dict):
+                info = [info]
+            for record in info:
+                _id = record['id']
+                if _id not in pathway_ids:
+                    pathway_ids.add(_id)
+                    yield {'_id': _id,
+                           'name': record['term'],
+                           'type': 'bp'}
+    # load molecular function
+    url = 'http://mygene.info/v3/query?q=_exists_:go.MF&fields=go.MF&fetch_all=TRUE'
+    cnt = 0
+    total = 1
+    pathway_ids = set()
+    while cnt < total:
+        doc = requests.get(url).json()
+        total = doc['total']
+        cnt += len(doc['hits'])
+        url = 'http://mygene.info/v3/query?scroll_id=' + doc['_scroll_id']
+        for _doc in doc['hits']:
+            info = _doc['go']['MF']
+            if isinstance(info, dict):
+                info = [info]
+            for record in info:
+                _id = record['id']
+                if _id not in pathway_ids:
+                    pathway_ids.add(_id)
+                    yield {'_id': _id,
+                           'name': record['term'],
+                           'type': 'mf'}
+    # load cellular component
+    url = 'http://mygene.info/v3/query?q=_exists_:go.CC&fields=go.CC&fetch_all=TRUE'
+    cnt = 0
+    total = 1
+    pathway_ids = set()
+    while cnt < total:
+        doc = requests.get(url).json()
+        total = doc['total']
+        cnt += len(doc['hits'])
+        url = 'http://mygene.info/v3/query?scroll_id=' + doc['_scroll_id']
+        for _doc in doc['hits']:
+            info = _doc['go']['CC']
+            if isinstance(info, dict):
+                info = [info]
+            for record in info:
+                _id = record['id']
+                if _id not in pathway_ids:
+                    pathway_ids.add(_id)
+                    yield {'_id': _id,
+                           'name': record['term'],
+                           'type': 'cc'}
